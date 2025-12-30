ece314feedd5f6c1333890037e1b16c9d762240f
Daniel Lopes <daniel@growthx.ai>
2025-12-30 15:49:51 -0800

    chore: add initial project scaffolding
    Add standard Ruby gem project files including gitignore,
    RuboCop config, license, and development scripts.

---
+++ lib/output_workflows.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "output_workflows/version"
+
+module OutputWorkflows
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "output_workflows"
+
+require "minitest/autorun"
